b740e1b632d8e6e32defe54e57435a30ae16b7f6
Dzevad <ynwa886@gmail.com>
2021-03-10 14:46:08 +0100

    Added index.js file

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -4,6 +4,8 @@ function main(numOne, numTwo) {
     //Only change code above this line    
     return sum;
 }
- console.log(main()); //change this line
-
+ console.log(main(15, 8));   //change this line
+ console.log(main(5, 28));
+ console.log(main(150, 425));
+ 
  module.experts = main();
