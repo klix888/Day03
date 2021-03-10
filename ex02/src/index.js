09898f63412939d0725f6f5f33426de369eb69f8
Dzevad <ynwa886@gmail.com>
2021-03-10 11:16:43 +0100

    Added index.js files

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+    //Dzevad
+
+    /*
+    D
+    z
+    e
+    v
+    a
+    d
+    */
+    //Only change code above this line 
+}
+ console.log(main());
+
+ module.experts = main();
